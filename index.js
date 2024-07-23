eddce31e33d8ecca98cebed66efaef629274e666
Artysuk <76599905+Artysuk@users.noreply.github.com>
2024-07-23 13:57:43 +0300

    up

---
+++ index.js
@@ -0,0 +1,48 @@
+
+//anonymous function
+//lol
+// for(let i = 0; i< document.querySelectorAll(".drum").length; i++){
+//     document.querySelectorAll(".drum")[i].addEventListener("click",function(){alert()});
+// }
+for(let i = 0; i< document.querySelectorAll(".drum").length; i++){
+    document.addEventListener("keypress",function(event){
+        makeSound(event.key)
+    });
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        makeSound(this.textContent);
+    });
+}
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            audio = new Audio("./sounds/crash.mp3");
+            this.innerHtml = audio.play();
+            break;
+        case "a":
+            audio = new Audio("./sounds/kick-bass.mp3");
+            this.innerHtml = audio.play();
+            break;
+        case "s":
+            audio = new Audio("./sounds/snare.mp3");
+            this.innerHtml = audio.play();
+            break;
+        case "d":
+            audio = new Audio("./sounds/tom-1.mp3");
+            this.innerHtml = audio.play();
+            break;
+        case "j":
+            audio = new Audio("./sounds/tom-2.mp3");
+            this.innerHtml = audio.play();
+            break;
+        case "k":
+            audio = new Audio("./sounds/tom-3.mp3");
+            this.innerHtml = audio.play();
+            break;
+        case "l":
+            audio = new Audio("./sounds/tom-4.mp3");
+            this.innerHtml = audio.play();
+            break;
+
+    }
+}
